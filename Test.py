3003772e129e21a4334e90005bc77bbfb5cf5ecb
Trungiuem00 <111987533+Trungiuem00@users.noreply.github.com>
2025-05-20 13:49:17 +0700

    Update Test.py

--- Test.py
+++ Test.py
@@ -6,7 +6,7 @@ from flask import Flask
 
 # ==== CẤU HÌNH ====
 CHANNEL_ID = os.getenv("CHANNEL_ID", "")
-DELAY_BETWEEN_TOKENS = 3  # giây
+DELAY_BETWEEN_TOKENS = 10  # giây
 
 # ==== LẤY TOKEN ====
 tokens_env = os.getenv("DISCORD_TOKENS", "")
